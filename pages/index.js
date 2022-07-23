b7d28bc031eaf4c26a32bf884bb0ea1a58a8fbd6
Alec Krafzik <alec.krafzik@grover.com>
2022-07-23 00:41:06 -0300

    fix: endure DOM loaded before rendering

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,43 @@
 import Head from "next/head";
-import '../i18n';
+import dynamic from "next/dynamic";
+import "../i18n";
 import { useTranslation } from "react-i18next";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const {t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
   const changeLanguage = (e) => {
     i18n.changeLanguage(e.target.value);
   };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>{t('head')}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <h1>{t('title')}</h1>
-        <select
-          onChange={changeLanguage}
-          defaultValue={i18n.resolvedLanguage}
-          className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
-        >
-          <option className="text-black" value="en">
-            EN
-          </option>
-          <option className="text-black" value="de">
-            DE
-          </option>
-        </select>
-      </body>
-    </div>
+    <>
+      {domLoaded && (
+      <div className={styles.container}>
+        <Head>
+          <title>{t("head")}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+          <h1>{t("title")}</h1>
+          <select
+            onChange={changeLanguage}
+            className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
+            defaultValue={i18n.resolvedLanguage}
+          >
+            <option className="text-black" value="en">
+              EN
+            </option>
+            <option className="text-black" value="de">
+              DE
+            </option>
+          </select>
+      </div>
+      )}
+    </>
   );
 }
